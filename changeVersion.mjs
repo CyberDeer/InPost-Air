c97df058f6787f9141636b1aeb6c21596e742063
Cezary Bober <ceski23@gmail.com>
2024-02-03 19:52:10 +0000

    chore: update repo config

--- changeVersion.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs/promises'
-
-const manifestPath = './custom_components/inpost_air/manifest.json'
-const manifest = await fs.readFile(manifestPath, 'utf-8')
-const newManifest = {
-    ...JSON.parse(manifest),
-    version: process.argv[2]
-}
-fs.writeFile(manifestPath, JSON.stringify(newManifest, undefined, 2))
