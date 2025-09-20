8190ce89ec062ced045253062b605039d485d2f5
Cezary Bober <ceski23@gmail.com>
2025-09-20 13:11:14 +0200

    chore: add API integration tests (#71)
    * chore: add API integration tests
    
    * chore: update tests
    
    * chore: skip some tests on CI

---
+++ tests/test_api.py
@@ -0,0 +1,68 @@
+import pytest
+import pytest_socket
+import os
+from custom_components.inpost_air.api import InPostApi
+from custom_components.inpost_air.models import (
+    InPostAirPoint,
+    InPostAirPointCoordinates,
+)
+
+
+@pytest.fixture()
+def _allow_inpost_requests():
+    pytest_socket.enable_socket()
+    pytest_socket.socket_allow_hosts(["inpost.pl"])
+
+
+@pytest.mark.parametrize("expected_lingering_timers", [True])
+@pytest.mark.api
+async def test_parcel_lockers_list(hass, _allow_inpost_requests):
+    response = await InPostApi(hass).get_parcel_lockers_list()
+    assert response is not None
+
+
+@pytest.mark.parametrize("expected_lingering_timers", [True])
+@pytest.mark.api
+async def test_parcel_locker_search(hass, _allow_inpost_requests):
+    response = await InPostApi(hass).search_parcel_locker("AJE01BAPP")
+    assert response is not None
+
+
+@pytest.mark.skipif(
+    os.environ.get("CI") == "true", reason="InPost blocks Github IP address"
+)
+@pytest.mark.parametrize("expected_lingering_timers", [True])
+@pytest.mark.api
+async def test_find_parcel_locker_id(hass, _allow_inpost_requests):
+    response = await InPostApi(hass).find_parcel_locker_id(
+        InPostAirPoint(
+            "AJE01BAPP",
+            1,
+            "Market Dino",
+            "",
+            "",
+            "006",
+            "Andrzejewo",
+            "andrzejewo",
+            "Warszawska",
+            "mazowieckie",
+            "07-305",
+            "62A",
+            "24/7",
+            "[]",
+            InPostAirPointCoordinates(52.83679, 22.20968),
+            0,
+            1,
+        )
+    )
+    assert response is not None
+
+
+@pytest.mark.skipif(
+    os.environ.get("CI") == "true", reason="InPost blocks Github IP address"
+)
+@pytest.mark.parametrize("expected_lingering_timers", [True])
+@pytest.mark.api
+async def test_air_data(hass, _allow_inpost_requests):
+    response = await InPostApi(hass).get_parcel_locker_air_data("AJE01BAPP", "56311")
+    assert response is not None
